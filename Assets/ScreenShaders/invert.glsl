e061c4937cc2720b734da765df9e4df46e04fdba
trq <80845920+tariqaq@users.noreply.github.com>
2026-09-11 07:48:08 +0800

    v1.3: make inversion shader intensity-aware

--- Assets/ScreenShaders/invert.glsl
+++ Assets/ScreenShaders/invert.glsl
@@ -5,8 +5,10 @@ in vec2 v_texcoord;
 uniform sampler2D tex;
 out vec4 fragColor;
 
+const float PERSONABOI_INTENSITY = 1.000;
+
 void main() {
     vec4 pixColor = texture(tex, v_texcoord);
-    fragColor = vec4(1.0 - pixColor.rgb, pixColor.a);
+    vec3 inverted = 1.0 - pixColor.rgb;
+    fragColor = vec4(mix(pixColor.rgb, inverted, PERSONABOI_INTENSITY), pixColor.a);
 }
-
